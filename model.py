8f9414bb56a6964cff01016b4a8382764b8e9ea3
Maciej Żyrek <prozyr@gmail.com>
2024-04-29 12:07:19 +0200

    fix

--- model.py
+++ model.py
@@ -110,6 +110,67 @@ class _UpsampleBlock(nn.Module):
     
 
 """ RDDBNet """""
+
+class _ResidualDenseBlock(nn.Module):
+    """Achieves densely connected convolutional layers.
+    `Densely Connected Convolutional Networks <https://arxiv.org/pdf/1608.06993v5.pdf>` paper.
+
+    Args:
+        channels (int): The number of channels in the input image.
+        growth_channels (int): The number of channels that increase in each layer of convolution.
+    """
+
+    def __init__(self, channels: int, growth_channels: int) -> None:
+        super(_ResidualDenseBlock, self).__init__()
+        self.conv1 = nn.Conv2d(channels + growth_channels * 0, growth_channels, (3, 3), (1, 1), (1, 1))
+        self.conv2 = nn.Conv2d(channels + growth_channels * 1, growth_channels, (3, 3), (1, 1), (1, 1))
+        self.conv3 = nn.Conv2d(channels + growth_channels * 2, growth_channels, (3, 3), (1, 1), (1, 1))
+        self.conv4 = nn.Conv2d(channels + growth_channels * 3, growth_channels, (3, 3), (1, 1), (1, 1))
+        self.conv5 = nn.Conv2d(channels + growth_channels * 4, channels, (3, 3), (1, 1), (1, 1))
+
+        self.leaky_relu = nn.LeakyReLU(0.2, True)
+        self.identity = nn.Identity()
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        out1 = self.leaky_relu(self.conv1(x))
+        out2 = self.leaky_relu(self.conv2(torch.cat([x, out1], 1)))
+        out3 = self.leaky_relu(self.conv3(torch.cat([x, out1, out2], 1)))
+        out4 = self.leaky_relu(self.conv4(torch.cat([x, out1, out2, out3], 1)))
+        out5 = self.identity(self.conv5(torch.cat([x, out1, out2, out3, out4], 1)))
+
+        x = torch.mul(out5, 0.2)
+        x = torch.add(x, identity)
+
+        return x
+
+class _ResidualResidualDenseBlock(nn.Module):
+    """Multi-layer residual dense convolution block.
+
+    Args:
+        channels (int): The number of channels in the input image.
+        growth_channels (int): The number of channels that increase in each layer of convolution.
+    """
+
+    def __init__(self, channels: int, growth_channels: int) -> None:
+        super(_ResidualResidualDenseBlock, self).__init__()
+        self.rdb1 = _ResidualDenseBlock(channels, growth_channels)
+        self.rdb2 = _ResidualDenseBlock(channels, growth_channels)
+        self.rdb3 = _ResidualDenseBlock(channels, growth_channels)
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        x = self.rdb1(x)
+        x = self.rdb2(x)
+        x = self.rdb3(x)
+
+        x = torch.mul(x, 0.2)
+        x = torch.add(x, identity)
+
+        return x
+
 class RRDBNet(nn.Module):
     def __init__(
             self,
@@ -206,39 +267,6 @@ class RRDBNet(nn.Module):
     def forward(self, x: Tensor) -> Tensor:
         return self._forward_impl(x)
 
-class _ResidualDenseBlock(nn.Module):
-    """Achieves densely connected convolutional layers.
-    `Densely Connected Convolutional Networks <https://arxiv.org/pdf/1608.06993v5.pdf>` paper.
-
-    Args:
-        channels (int): The number of channels in the input image.
-        growth_channels (int): The number of channels that increase in each layer of convolution.
-    """
-
-    def __init__(self, channels: int, growth_channels: int) -> None:
-        super(_ResidualDenseBlock, self).__init__()
-        self.conv1 = nn.Conv2d(channels + growth_channels * 0, growth_channels, (3, 3), (1, 1), (1, 1))
-        self.conv2 = nn.Conv2d(channels + growth_channels * 1, growth_channels, (3, 3), (1, 1), (1, 1))
-        self.conv3 = nn.Conv2d(channels + growth_channels * 2, growth_channels, (3, 3), (1, 1), (1, 1))
-        self.conv4 = nn.Conv2d(channels + growth_channels * 3, growth_channels, (3, 3), (1, 1), (1, 1))
-        self.conv5 = nn.Conv2d(channels + growth_channels * 4, channels, (3, 3), (1, 1), (1, 1))
-
-        self.leaky_relu = nn.LeakyReLU(0.2, True)
-        self.identity = nn.Identity()
-
-    def forward(self, x: Tensor) -> Tensor:
-        identity = x
-
-        out1 = self.leaky_relu(self.conv1(x))
-        out2 = self.leaky_relu(self.conv2(torch.cat([x, out1], 1)))
-        out3 = self.leaky_relu(self.conv3(torch.cat([x, out1, out2], 1)))
-        out4 = self.leaky_relu(self.conv4(torch.cat([x, out1, out2, out3], 1)))
-        out5 = self.identity(self.conv5(torch.cat([x, out1, out2, out3, out4], 1)))
-
-        x = torch.mul(out5, 0.2)
-        x = torch.add(x, identity)
-
-        return x
 
 """ CycleNet """
 class CycleNet(nn.Module):
